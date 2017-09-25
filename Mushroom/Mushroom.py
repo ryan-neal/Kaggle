fb52bd3b619ac3e752e116dcc248f2c47c2104e6
ryan-neal <ryan.neal0@gmail.com>
2017-09-25 18:08:11 -0500

    Cleaned mushroom.py code

--- Mushroom/Mushroom.py
+++ Mushroom/Mushroom.py
@@ -1,14 +1,7 @@
-
-# coding: utf-8
-
-# In[62]:
-
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-#get_ipython().magic('matplotlib inline')
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.ensemble import RandomForestClassifier
@@ -16,24 +9,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import tree
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline
+from IPython.display import Image
+from sklearn.externals.six import StringIO
+from sklearn.tree import export_graphviz
+import pydotplus
 
 
-# In[59]:
-
+# Read mushroom data
 
 mushroom_df = pd.read_csv("mushrooms.csv")
 
-
-# In[3]:
-
-
+# Convert data frame to categorical variables
 def get_features(data):
     features = [f for f in data]
     return features
-
-
-# In[4]:
-
 
 def make_categories(data):
     features = get_features(data)
@@ -42,16 +31,9 @@ def make_categories(data):
         data[feature] = data[feature].cat.codes
     return data
 
-
-# In[70]:
-
-
 mushroom = make_categories(mushroom_df)
 
-
-# In[77]:
-
-
+# Check to see if any features have only 1 value. Drop those features
 features = get_features(mushroom)
 empty_dict = {}
 for feature in features:
@@ -59,48 +41,24 @@ for feature in features:
 print(empty_dict)
 
 
-# In[78]:
-
-
 mushroom.drop("veil-type", axis=1, inplace=True)
 
 
-# In[6]:
+# Create a correlation heatmap
+print(sns.heatmap(mushroom.corr()))
 
-
-sns.heatmap(mushroom.corr())
-
-
-# In[7]:
-
-
+# Sort correlations
 mushroom_corr = mushroom.corr()
-
-
-# In[8]:
-
-
 print(mushroom_corr.sort_values(["class"], ascending=False))
 
-
-# In[82]:
-
-
+# Separate features and target
 X = mushroom.drop("class", axis=1)
 y = mushroom["class"]
 
-
-# In[83]:
-
-
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-
-# ## Random Forest
-
-# In[12]:
-
-
+# Random Forest Model
 forest = RandomForestClassifier()
 forest_params = {"n_estimators": [1, 3, 6, 10, 30, 60, 100, 300, 600, 1000],
                 "criterion": ["gini", "entropy"],
@@ -108,42 +66,13 @@ forest_params = {"n_estimators": [1, 3, 6, 10, 30, 60, 100, 300, 600, 1000],
 forest_grid = GridSearchCV(forest, forest_params, cv=10)
 forest_grid.fit(X_train, y_train)
 
-
-# In[14]:
-
-
 forest_grid.best_params_
-
-
-# In[15]:
-
-
 forest_grid.best_score_
-
-
-# In[16]:
-
-
 y_pred = forest_grid.predict(X_test)
-
-
-# In[20]:
-
-
 print(classification_report(y_pred, y_test))
-
-
-# In[22]:
-
-
 print(confusion_matrix(y_pred, y_test))
 
-
-# ## Logistic Regression
-
-# In[24]:
-
-
+# GridSearchCV Logistic Regression Model
 logistic = LogisticRegression()
 logistic_params = {"penalty": ["l1", "l2"],
                 "C": [1, 3, 6, 10, 30, 60, 100, 300, 600, 1000]}
@@ -151,73 +80,37 @@ logistic_grid = GridSearchCV(logistic, logistic_params, cv=10)
 logistic_grid.fit(X_train, y_train)
 
 
-# In[25]:
-
-
 logistic_grid.best_params_
-
-
-# In[27]:
-
-
 logistic_grid.best_score_
 
-
-# In[57]:
-
-
+# Non GridSearchCV Logistic Regression (for coefficient analysis)
 logistic2 = LogisticRegression(penalty="l1", C=11)
 logistic2.fit(X_train, y_train)
 y_pred = logistic2.predict(X_test)
+
+# Sort most predictive coefficients
 X_features = get_features(X)
 coefficients = logistic2.coef_.transpose()
 coeff_df = pd.DataFrame(coefficients, index=X_features, columns= ["importance"])
 coeff_df.sort_values("importance")
 
-
-# In[40]:
-
-
 print(classification_report(y_pred, y_test))
 
 
-# ## Logistic Regression with SelectKBest Pipeline
-
-# In[84]:
-
-
+# Logistic Regression with SelectKBest Pipeline
 five_best = SelectKBest(k=5)
 top_five_log = Pipeline(steps=[("five_best", five_best), ("logistic2", logistic2)])
-
-
-# In[85]:
-
-
 top_five_log.fit(X_train, y_train)
-
-
-# In[86]:
-
-
 y_pred2 = top_five_log.predict(X_test)
-
-
-# In[87]:
-
-
-y_pred2
-
-
-# In[88]:
-
-
 print(classification_report(y_pred2, y_test))
-
-
-# In[89]:
-
-
 print(confusion_matrix(y_pred2, y_test))
 
-
-# In[ ]:
+## Decision tree
+d_tree = tree.DecisionTreeClassifier(criterion="gini")
+d_tree.fit(X_train, y_train)
+y_tree_pred = d_tree.predict(X_test)
+print(classification_report(y_tree_pred, y_test))
+dot_data = StringIO()
+dot_data = tree.export_graphviz(d_tree, out_file=None,feature_names=X_features)
+graph = pydotplus.graph_from_dot_data(dot_data)
+Image(graph.create_png())
